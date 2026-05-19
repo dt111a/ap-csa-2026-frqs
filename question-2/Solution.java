eb2b53c3f5b1c3c3f12104b3673261dd1d898c12
dt111a <darrentruong41@gmail.com>
2026-05-19 11:22:10 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,17 @@
+public class Bottle {
+    double capacity; 
+    double currentAmount; 
 
+    public Bottle(double x) {
+        capacity = x; 
+        currentAmount = capacity; 
+    }
+
+    public double updateAmount(double change) {
+        currentAmount -= change; 
+        if (currentAmount < (capacity*0.25)) {
+            currentAmount = capacity; 
+        }
+        return currentAmount; 
+    }
+}
