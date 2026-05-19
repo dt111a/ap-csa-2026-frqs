ee21c23174f234885d11250a81ef525aa0ae81d6
dt111a <darrentruong41@gmail.com>
2026-05-19 11:23:17 -0400

    Update Solution.java

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,15 @@
-
+public int getPointsForRow(int targetRow) {
+    boolean word = true; 
+    int sum =0; 
+    for (int j = 0; j < board[0].length; j++){ 
+        sum+= board[targetRow][j].getPoints(); 
+        String color = board[targetRow][0].getColor(); 
+        if (!(board[targetRow][j].getColor().equals(color))){
+            word = false; 
+        }
+    }
+    if (word) {
+        sum*=2; 
+    }
+    return sum; 
+}
