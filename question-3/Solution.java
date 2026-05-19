f9b9e7f40f7effd1762dffe69283ee735336004b
dt111a <darrentruong41@gmail.com>
2026-05-19 11:23:03 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,1 +1,19 @@
+public int moreHistoryThanMathAbsences() { 
+    int count = 0;
+    for (int i = 0; i<historyList.size(); i++) {
+        CourseRecord student = historyList.get(i); 
+        int numAbsences = student.getAbsences(); 
+        String name = student.getStudentID(); 
 
+        for(int j = 0; j < mathList.size(); j++) {
+            CourseRecord studentTwo = mathList.get(j); 
+            int numAbsencesTwo = studentTwo.getAbsences(); 
+            String nameTwo = studentTwo.getStudentID();
+            if ((nameTwo.equals(name))&&(numAbsences>numAbsencesTwo)) {
+                count++; 
+            }
+
+        }       
+    }
+    return count;
+}
