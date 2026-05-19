30047cd42e4b6a65d6e4d29d7c6633736d056f8f
dt111a <darrentruong41@gmail.com>
2026-05-19 11:22:44 -0400

    Update Solution.java

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,23 @@
+public Account(String requestedName) {
+    if (isAvailable(requestedName)) {
+        username = requestedName; 
+    } else {
+        String copy = requestedName; 
+        requestedName = requestedName + "1";
+        int i = 2; 
+        while(!(isAvailable(requestedName))) {
+            requestedName = copy + i; 
+            i++; 
+        }
+        username = requestedName; 
+    }
+}
 
+public String getShortenedName() {
+    String newWord = username; 
+    while(newWord.indexOf("-")!=-1) {
+        int index = newWord.indexOf("-");
+        newWord = newWord.substring(0,index-1) + newWord.substring(index+1); 
+    }
+    return newWord; 
+}
